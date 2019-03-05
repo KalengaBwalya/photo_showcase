47bd69c84156d09983425c4cc62adc8ef9f32511
Kalenga Bwalya <kalengabwalya@gmail.com>
2019-03-05 20:15:49 +0200

    implemnts the $database object

--- includes/database.php
+++ includes/database.php
@@ -16,7 +16,7 @@ class MySQLDatabase {
         $this->open_connection();
     }
 
-    public function open_connection(){
+    public function open_connection() {
         $this->connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS);
         if(!$this->connection){
             die("DATABASE CONNECTION FAILED ".mysqli_error($this->connection));
@@ -28,20 +28,20 @@ class MySQLDatabase {
         }
     }
 
-    public function  close_connection(){
+    public function  close_connection() {
         if(isset($this->connection)){
             mysqli_close($this->connection);
         }
         unset($this->connection);
     }
 
-    public function query($sql){
-        $result = mysqli_query($sql, $this->connection);
+    public function query($sql) {
+        $result = mysqli_query($this->connection, $sql);
         $this->confirm_query($result);
         return $result;
     }
 
-    public function mysql_prep ($value) {
+    public function escape_value ($value) {
         $magic_quotes_active = get_magic_quotes_gpc();
         $new_enough_php = function_exists("mysqli_real_escape_string"); //i.e. PHP >= v4.3.0
         if($new_enough_php) { //PHP v4.3.0 or higher
@@ -49,7 +49,7 @@ class MySQLDatabase {
             if($magic_quotes_active) {
                 $value = stripslashes($value);
             }
-            $value = mysqli_real_escape_string($value);
+            $value = mysqli_real_escape_string($this->connection, $value);
         } else { // before PHP v4.3.0
             //if magic quotes aren't already an the add slashes manually
             if(!$magic_quotes_active) {
@@ -60,7 +60,25 @@ class MySQLDatabase {
         return $value;
     }
 
-    private function confirm_query($result){
+    //"database-neutral" methods
+    public function fetch_array($result_set) {
+        return mysqli_fetch_array($result_set);
+    }
+
+    public function num_rows($result_set) {
+        return mysqli_num_rows($result_set);
+    }
+
+    public function insert_id() {
+        //get the last id inserted over the current db connection
+        return mysqli_insert_id($this->connection);
+    }
+
+    public function affected_rows() {
+        return mysqli_affected_rows($this->connection);
+    }
+
+    private function confirm_query($result) {
         if(!$result){
             die("DATABASE QUERY FAILED:".mysqli_error($this->connection));
         }
